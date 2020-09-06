54fc5f9902af109dcf4035956b261a9a0897e0fe
arlenemccann <70239596+arlenemccann@users.noreply.github.com>
2020-09-06 16:05:26 -0400

    Working on my project

---
+++ lookingOutForNumberOne.java
@@ -0,0 +1,56 @@
+public class lookingOutForNumberOne {
+
+
+    public static void countDigits(int count, int num){
+
+        int count = 0, num = 123423876;
+        for (; num != 0; num = num/10, ++count) {
+        } //Goal: Count the number of digits in an integer
+        //if the number doesn't equal zero, an
+
+
+    }
+
+    public static int nthDigitBack(int n, int num){
+        for ( int i =0; i < 10; i++){
+
+
+        }
+
+
+
+
+        return 0;
+    } //Goal: Find the location of a digit from the right to left
+
+
+    public static int nthDigit(int n, int num) {
+
+
+        return 0;
+
+    }  //Goal: Find the location of a digit from left to right
+
+
+    public static void updateTally(int n, int num, int[] tally){
+
+
+        
+
+
+    } //Goal: Write a method that calls nthDigit, and takes in 3 arguments:
+    //an int (n), an int (num), and an int[] tally. We assume that tall is an int[] of 10 integers.
+    //The tally contains the tally for the number of nth digits seen so far.
+    //It updates the tall to reflect the nth digit of num.
+
+
+    public static void main(String[] args) {
+
+        System.out.println(countDigits);
+
+        System.out.println(countDigits.nthDigit(12342356,5));
+
+    }
+
+
+}
