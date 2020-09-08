37fcc6c7c7b839f08120f1009ce5e937950cd092
arlenemccann <70239596+arlenemccann@users.noreply.github.com>
2020-09-08 15:57:31 -0400

    Update lookingOutForNumberOne.java
    Updated countDigits to an int, and then started trying to get into the nthDigitBack thing

--- lookingOutForNumberOne.java
+++ lookingOutForNumberOne.java
@@ -1,43 +1,32 @@
 public class lookingOutForNumberOne {
 
 
-    public static void countDigits(int num){
+    public static int countDigits(int num){
 
         int count = 0;
         for (; num != 0; num = num/10, ++count) {
         } //Goal: Count the number of digits in an integer
         //if the number doesn't equal zero, an
-        
+
         return count;
 
 
     }
 
-    public static int nthDigitBack(int n, int num){
-        for ( int i =0; i < 10; i++){
+    public static int nthDigitBack(int n, int digit){
+        if (n % 10 == digit){
+            }
 
-
-        }
-
-
-
-
-        return 0;
+        return n;
     } //Goal: Find the location of a digit from the right to left
 
-
     public static int nthDigit(int n, int num) {
-
 
         return 0;
 
     }  //Goal: Find the location of a digit from left to right
 
-
     public static void updateTally(int n, int num, int[] tally){
-
-
-        
 
 
     } //Goal: Write a method that calls nthDigit, and takes in 3 arguments:
@@ -47,9 +36,11 @@ public class lookingOutForNumberOne {
 
 
     public static void main(String[] args) {
-        
-        int count= countDigits(12345678);
+
+        int count = countDigits(12345678);
         System.out.println(count);
+        int position = nthDigitBack(12345678, 4);
+        System.out.println(position);
 
     }
 
