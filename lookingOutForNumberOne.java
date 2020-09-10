a7b1eb5c3793ef91bfea24d4d7d40596dda2802b
arlenemccann <70239596+arlenemccann@users.noreply.github.com>
2020-09-10 11:14:32 -0400

    Update lookingOutForNumberOne.java
    Update the rest of the methods and comments. Still broken, but current state.

--- lookingOutForNumberOne.java
+++ lookingOutForNumberOne.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class lookingOutForNumberOne {
 
 
@@ -5,51 +9,89 @@ public class lookingOutForNumberOne {
 
         int count = 0;
         for (; num != 0; num = num/10, ++count) {
-        } //Goal: Count the number of digits in an integer
-        //if the number doesn't equal zero, an
+        }
 
         return count;
+    }//Part 1 - Goal: Count the number of digits in an integer
+    public static char nthDigitBack(int n, int num){
 
-
-    }
-    public static char nthDigitBack(int a, int b){
-
-        String z = a + "";
+        String z = num + "";
         char z1[] = z.toCharArray();
         int length = z1.length - 1;
-        char result = z1[length - b];
+        char result = z1[length - n];
         return result;
 
     }
-    //Goal: Find the nth Digit from right to left
+    //Part 2 - Goal: Find the nth Digit from right to left
 
-    public static char nthDigit(int x, int y){
-        String digit = x + "";
-        return digit.charAt(y);
+    public static char nthDigit(int n, int num){
+        String digit = num + "";
+        return digit.charAt(n);
 
     }
-
-    //Goal: Find the nth Digit from left to right
+    //Part 3 - Goal: Find the nth Digit from left to right
 
 
 
     public static void updateTally(int n, int num, int[] tally){
+        int index = nthDigit(n,num);
+        int updateInt = 0;
+        for(int i = 0; i <= n; i++){
+            updateInt += tally[i];
+        }
+        tally[index] = updateInt;
 
-
-    } //Goal: Write a method that calls nthDigit, and takes in 3 arguments:
+    } //Part 4 - Goal: Write a method that calls nthDigit, and takes in 3 arguments:
     //an int (n), an int (num), and an int[] tally. We assume that tall is an int[] of 10 integers.
     //The tally contains the tally for the number of nth digits seen so far.
     //It updates the tall to reflect the nth digit of num.
 
+    public static int[] nthDigitTally(int n, int[] nums){
+        int[] tally = new int[10];
+        for(int i = 0; i < nums.length; i++){
+            updateTally(n, i, tally);
+        }
+
+      return tally;
+    } //Part 5 - Goal: Write a method that calls updateTally, and takes in 2 arguments:
+    //and returns a tally of frequencies of 0-9 as the nth digit of all the numbers in nums
+
+
+    public static int[] readMysteriousNumbers(File filename){
+        File file = new File("livejournal.txt");
+        Scanner scanner = new Scanner(filename);
+        String[] fileInput = filename.list();
+        int[] nums = new int[fileInput.length];
+        for(int i = 0; i < fileInput.length;i++){
+            nums[i] = Integer.parseInt(fileInput[i]);
+        }
+
+        return nums;
+    }//Part 6 - Goal: Write a method that reads whitespace separated integers from a file
+    //and returns a list of numbers suitable as input to nthDigitTally
+
+
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String a = input.nextLine();
+        int b = Integer.parseInt(a);
+        String file = input.nextLine();
+        File readFile = new File(file);
+
         try {
 
+            int[] nums = readMysteriousNumbers(file);
+            int[] tally = nthDigitTally(b, nums);
+            for(int i = 0; i < 10; i++){
+                System.out.println(i + "s:\t" + tally[i]);
+            }
 
-        int count = countDigits(12345678);
-        System.out.println(count);
-        System.out.println(nthDigitBack(8675309, 4));
-        System.out.println(nthDigit(123453232, 6));
+        //int count = countDigits(12345678);
+        //System.out.println(count);
+        //System.out.println(nthDigitBack(0, 123));
+        //System.out.println(nthDigit(0, 123));
+
     }
         catch (Exception e){
             System.out.println("Oops. Something went wrong.");
